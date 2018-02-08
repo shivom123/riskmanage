bacc59c93fe1e7cfed61f3c8e30c107b3a491778
admin <khatrihimanshu581@gmail.com>
2018-02-08 21:16:49 +0530

    final_riskmanagement

--- RiskManagement/settings.py
+++ RiskManagement/settings.py
@@ -119,7 +119,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-heroku config:set DEBUG_COLLECTSTATIC=1
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
